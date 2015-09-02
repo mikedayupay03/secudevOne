c10f7ab48d9b7288a726998e384e399e84889654
Mike Dayupay <mike.dayupay03@gmail.com>
2015-09-02 12:27:14 +0800

    added css file

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <title>Login</title>
   </head>
+  <link rel="stylesheet" type="text/css" href="/css/main.css" charset="utf-8">
   <body>
     <form method="post" action="logged.php">
       Username: <input type="text" name="username"><br>
