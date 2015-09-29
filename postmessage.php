67f90050b9f234ca1e26fcc593a88fbab823ab90
markgromantigue <markg.romantigue@gmail.com>
2015-09-29 12:33:28 +0800

    Added XSS and Injection Protection for Messaging

--- postmessage.php
+++ postmessage.php
@@ -1,6 +1,12 @@
 <?php
 
   session_start();
+  
+  require_once '/htmlpurifier-4.7.0/library/HTMLPurifier.auto.php';
+
+  $config = HTMLPurifier_Config::createDefault();
+  $purifier = new HTMLPurifier($config);
+    
   mysql_connect("localhost","root","1234") or die (mysql_error());
   mysql_select_db("secudev1") or die (mysql_error());
 
@@ -9,9 +15,10 @@
   $sql = "SELECT user_id FROM userdb WHERE username = '" . $myusername . "'";
   $userId = mysql_fetch_array(mysql_query($sql));
 
-
-
-  $insertMessageQuery = "INSERT INTO message_board(user_id, message, date_posted) VALUES ('" . $userId[0] . "', '" . $_POST['message'] . "', CURRENT_TIMESTAMP)";
+  //To protect from XSS attacks and Mysql Injection
+  $message = mysql_real_escape_string($_POST["message"] );
+  $message = $purifier->purify($message);
+  $insertMessageQuery = "INSERT INTO message_board(user_id, message, date_posted) VALUES ('" . $userId[0] . "', '" . $message . "', CURRENT_TIMESTAMP)";
   $result = mysql_query($insertMessageQuery);
   if ($result) {
     echo "success!";
