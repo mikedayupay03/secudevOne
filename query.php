5c7d348b701576c6702a6bb2f5883e72d7264a7e
renzdimayuga <renz574@gmail.com>
2015-10-12 12:41:53 +0800

    query user update

--- query.php
+++ query.php
@@ -4,11 +4,19 @@
 	if(!isset($_SESSION['myusername'])){ //if login in session is not set
     header("Location:index.php");
 	}
-
+	$n = $_POST["suser"];
 	mysql_connect("localhost","root","1234") or die (mysql_error());
 	mysql_select_db("secudev1") or die (mysql_error());
 	$x = $_POST["squery"];
-	$strSQL = "SELECT a.message,a.date_posted,b.username FROM message_board a,userdb b WHERE a.user_id = b.user_id AND LOWER(a.message) LIKE '%" . strtolower($x) . "%'";
+	if (count($n) > 0) {
+		$strSQL = "SELECT a.message,a.date_posted,b.username FROM message_board a,userdb b WHERE a.user_id = b.user_id AND LOWER(a.message) LIKE '%" . strtolower($x) . "%' AND (";
+		for ($a = 0 ; $a < count($n) ; $a++) {
+			$strSQL = $strSQL . "b.username = '" . $n[$a] . "' OR ";
+		}
+		$strSQL = $strSQL . "0)";
+	} else {
+		$strSQL = "SELECT a.message,a.date_posted,b.username FROM message_board a,userdb b WHERE a.user_id = b.user_id AND LOWER(a.message) LIKE '%" . strtolower($x) . "%'";
+	}
 	$rs = mysql_query($strSQL);
 	echo "<table border=1 style=width:100%>\n<tr>\n<td>Message</td>\n<td>Date posted</td>\n<td>User</td>\n</tr>\n";
 	while ($row = mysql_fetch_array($rs)) {
