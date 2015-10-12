3d1e2ceeb9b62d61c478e696f6321d2c2a956121
markgromantigue <markg.romantigue@gmail.com>
2015-10-12 12:26:47 +0800

    UI fixes search

--- search.php
+++ search.php
@@ -12,9 +12,9 @@ $(document).ready(function(){
         document.getElementById("temp").innerHTML = "";
 		var newDiv = document.createElement('div');
 		if (this.value == 'between') {
-			document.getElementById("temp").innerHTML = "<input type=text name=date1> AND <input type=text name=date2>";
+			document.getElementById("temp").innerHTML = "<input type=date name=date1> AND <input type=date name=date2>";
 		} else {
-			document.getElementById("temp").innerHTML = "<input type=text name=date1>";
+			document.getElementById("temp").innerHTML = "<input type=date name=date1>";
 		}
         
 		// document.getElementById("testing").appendChild(newDiv);
@@ -34,7 +34,7 @@ function addUsers(name) {
 }
 function addDates(name) {
 	var newDiv = document.createElement('div');
-	newDiv.innerHTML = "<select class='sdate' id=sdate" + b +"><option value=between>Between</option><option value=earlier>Earlier</option><option value=later>Later</option><option value=during>During</option></select> <div id=temp><input type=text> AND <input type=text></div>";
+	newDiv.innerHTML = "<select class='sdate' id=sdate" + b +"><option value=between>Between</option><option value=earlier>Earlier</option><option value=later>Later</option><option value=during>During</option></select> <div id=temp><input type=date> AND <input type=date></div>";
 	document.getElementById("testing").appendChild(newDiv);
 	b++;
 }
@@ -43,7 +43,7 @@ function addDates(name) {
 <body>
 <form method=post action=query.php>
 Search query: <input type=text name=squery><br>
-<div id=testing></div>
+<div id=testing></div><br>
 <div id=testing2></div>
 <input type=button value='Specify dates' onClick=addDates('testing')>
 <input type=button value='Specify users' onClick=addUsers('testing')>
