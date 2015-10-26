d575d629844582a525633afe5c85e9c32dea3511
zachysoba <zachraduban@gmail.com>
2015-10-26 12:14:17 +0800

    links for store and cart and store.php blah

--- logged.php
+++ logged.php
@@ -131,7 +131,7 @@
             <input type="hidden" name="logout" id="logout"/>
         </form>
 		<header>
-			<h1>WELCOME <?php echo $row[1] . " " . $row[2] ?>!</h1>
+			<h1>WELCOME <?php echo $row[1] . " " . $row[2] ?>! <a href=""><img align="right" src= "res/cart.png" width="95" height="50"></a><a href=""><img align="right" src= "res/store.png" width="95" height="50"></h1></a>
 		</header>
 
 		<div class="container">
---
+++ store.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/landing-page.css" charset="utf-8">
+		<script src="js/jquery.min.js"></script>
+		<script type="text/javascript">
+	</head>
+	
+	<body>
+	</body>
+</html>
