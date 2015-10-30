23384290940f98f20ad05b8bd7490c0d356a4d9a
markgromantigue <markg.romantigue@gmail.com>
2015-10-30 16:14:50 +0800

    Removed ajax from store.php
    it is unnecessary

--- store.php
+++ store.php
@@ -34,78 +34,6 @@
 		<title>SECUDEV: Store</title>
 		<link rel="stylesheet" href="css/landing-page.css" charset="utf-8">
 		<script src="js/jquery.min.js"></script>
-		<script type="text/javascript">
-		$(document).ready(function(){
-			loadstation();
-		});
-		function loadstation(){
-			$.ajax({
-				url: "getitems.php",
-				type: "GET",
-				success: function(response) {
-					$("#message_container").html(response);
-					setTimeout(loadstation,5000);
-				}
-			});
-		}
-		</script>
-        <script>
-            function logoutFunction(){
-                $("input#logout").val(1);
-                // alert($("input#logout").val());
-                $("form#logoutForm").submit();
-            }
-        </script>
-        <script>
-        $(document).ready(function(){
-			$("#advanced").hide();
-            $("#hideButton").hide();
-		});
-        
-        function toggle (advanced){
-            if(advanced){
-                $("#advanced").show();
-                $("#advancedButton").hide();
-                $("#hideButton").show();
-            } else{
-                $("#advanced").hide();
-                $("#advancedButton").show();
-                $("#hideButton").hide();
-            }
-        }
-        
-        </script>
-        <script>
-            var a = 0;
-            var b = 0;
-            var elem = 1;
-            /*function moreDates() {
-                elem = this;
-                var newDiv = document.createElement('div');
-                alert(elem.value);
-            }*/
-            function addUsers(name) {
-                var newDiv = document.createElement('div');
-                newDiv.innerHTML = "<div class='sname'><select name='cond'><option value=AND>AND</option><option value=OR>OR</option></select> &nbsp; Input user " + a + ": <input type=text name=suser[]></div><br>";
-                document.getElementById("testing2").appendChild(newDiv);
-                a++;
-            }
-            function addDates(name) {
-                var newDiv = document.createElement('div');
-                newDiv.innerHTML = "<br><div id=container><div id=temp2 style= display:inline;><select name='cond'><option value=AND>AND</option><option value=OR>OR</option></select> <select id=doption" + b + "  name=doption[] onchange=myFunction('doption" + b + "','hider" + b + "')><option value=1>Between</option><option value=2>Earlier</option><option value=3>Later</option><option value=4>During</option></select></div> <div id=temp style= display:inline;><input type=date name='d0[]'> <input type=date id=hider" + b + " name=d1[]></div></div>";
-                document.getElementById("testing").appendChild(newDiv);
-                b++;
-            }
-            
-            function myFunction(name1,name2) {
-                var x = document.getElementById(name1).value;
-                if (x == 1) {
-                    document.getElementById(name2).style.visibility = "visible";
-                } else {
-                    document.getElementById(name2).style.visibility = "hidden";
-                }
-            }
-    </script>
 	</head>
 	<body>
         <form id="logoutForm" action="logout.php" method="POST">
@@ -124,18 +52,53 @@
 					echo "<a href='additem.php'><button class='btn' type='button'><strong><center>Add Item</center></strong></button></a>";
 					echo "</td>";
 				}
-			?>
-			<div>
-                <button type="button" id="hideButton" style="position:absolute; left:375px; top: 505px;" onclick="toggle(0)">Hide Advanced Search</button>
-                <div id="advanced">
-                <div id="testing"></div><br>
-                <div id="testing2"></div>
-                    <input type="button" value='Specify dates' onClick=addDates('testing')>
-                    <input type="button" value='Specify users' onClick=addUsers('testing')>
+              $sql = "SELECT * FROM items";
+              $query="SELECT item_id, item_name, item_description, item_image, item_price FROM items WHERE 1";
+              $rs = mysql_query($query);
+              $items = mysql_fetch_row($rs);
+              // if($items) {
+              //   echo "success!";
+              // }else {
+              //   echo mysql_error();
+              // }
 
-                </div>
-                </form>
-			</div>
+                echo "<table border = '1' col = >";
+              do {
+
+
+                echo "<tr>
+                <td>";
+
+               echo "<a href='#?item_id=". $items[0] ."'>" . $items[1] . "</a>" . "<br>";//name of item
+               echo "<b>P</b>" . $items[4] . "<br>";//price of item
+               
+               if(isset($_GET['item_id'])){
+               $itemId = $_GET['item_id'];
+               $query="SELECT item_image FROM items WHERE item_id = '" . $itemId . "'";
+               $result=mysql_query($query);
+               }?>
+          
+               <img src = "item_images/<?php echo $items[3];?>"  width="300" height="300" class="itemImage">
+               
+                <?php
+               echo "<br>" . $items[2] . "<br>";//description of item
+
+               echo "</td>";
+               echo "<td>";
+               echo "<a href='cart.php?item_id=".$items[0]."'><button class='btn' type='button'><strong><center>Add to Cart</center></strong></button></a>";
+               echo "</td>";
+               echo "<td>";
+               if($row[0] == $items[1] || $row['admin'] == 1){
+                    echo "<a href='edititem.php?item_id=".$items[0]."'><button class='btn' type='button'><strong><center>Edit</center></strong></button></a>";
+                    echo "</td>";
+                    echo "<td>";
+                    echo "<a href='store.php?item_id=".$items[0]."'" ?> onclick="return confirm('Are you sure you want to delete this message?')";<?php echo "><button class='btn' type='button'><strong><center>Delete</center></strong></button></a>";
+                }
+                echo "</td>";
+                echo "</tr>";
+
+              } while ($items = mysql_fetch_row($rs));
+              echo "</table>"; ?>
 			<hr>
 			<div id="message_container">
 
