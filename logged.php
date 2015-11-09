7b9344e65cffaf8ba06c7381e861b1432efd622b
Mike Dayupay <mike.dayupay03@gmail.com>
2015-11-09 09:35:51 +0800

    fixed donations button url redirects

--- logged.php
+++ logged.php
@@ -244,6 +244,7 @@
 				<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 			</form>
 
+
 			<h4>Donate P5</h4>
 		</div>
 
@@ -255,6 +256,7 @@
 				<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 			</form>
 
+
 			<h4>Donate P10</h4>
 		</div>
 
@@ -265,6 +267,7 @@
 				<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 				<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 			</form>
+
 			<h4>Donate P20</h4>
 		</div>
 
